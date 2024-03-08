a1a6d4b57a964d513362645ced3e70ea39dcee0a
Jay Em <jaybacs4@gmail.com>
2024-03-08 19:13:43 +0800

    initial commit

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,6 @@
+{{-- <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      Developed By Boss JayEm the Great
+    </div>
+    <strong>&copy; <?php echo date('Y')?> - Point of Sale System</strong> 
+  </footer>  --}}
